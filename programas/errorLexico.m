a0da26464d41d9f28db8440a772e143d872d5db3
nmigueles <nicolasmigueles99@gmail.com>
2024-11-23 20:23:43 -0300

    fixes y programas

---
+++ programas/errorLexico.m
@@ -0,0 +1,8 @@
+inicio
+    $1
+    leer (a, b);
+    cc := a + (b - 2);
+    escribir ("Imprime cc:", cc);
+    r := random(a, b);
+    escribir ("Imprime r:", r);
+fin
---
+++ programas/errorSemantico.m
@@ -0,0 +1,4 @@
+inicio
+    leer (a, b);
+    escribir (c);
+fin
